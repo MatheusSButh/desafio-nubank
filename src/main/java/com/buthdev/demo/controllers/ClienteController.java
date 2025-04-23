b382161bc0b9b490b34070ba788419a8b6ab8299
Matheus <matheusdsouzabuth@gmail.com>
2025-04-23 16:32:05 -0300

    findContactsByCliente endpoint

--- src/main/java/com/buthdev/demo/controllers/ClienteController.java
+++ src/main/java/com/buthdev/demo/controllers/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.buthdev.demo.dto.ClienteDTO;
 import com.buthdev.demo.dto.ClienteResponseDTO;
+import com.buthdev.demo.dto.ContatoResponseDTO;
 import com.buthdev.demo.model.Cliente;
 import com.buthdev.demo.services.ClienteService;
 
@@ -32,4 +34,10 @@ public class ClienteController {
 	public ResponseEntity<List<ClienteResponseDTO>> findAll() {
 		return ResponseEntity.ok().body(clienteService.findAll());
 	}
+	
+	@GetMapping(value = "/{id}/contatos")
+	public ResponseEntity<List<ContatoResponseDTO>> findContactsByCliente(@PathVariable Long id) {
+		return ResponseEntity.ok().body(clienteService.findContactsByCliente(id));
+	}
+	
 }
